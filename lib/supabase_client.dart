1548b840c878019639abb16339b012dffdbbb6b2
YuzuuDev <ddscarletdd@gmail.com>
2025-12-13 08:42:32 +0800

    Update supabase_client.dart

--- lib/supabase_client.dart
+++ lib/supabase_client.dart
@@ -5,8 +5,8 @@ class SupabaseClientInstance {
 
   static Future<void> init() async {
     await Supabase.initialize(
-      url: 'YOUR_SUPABASE_URL',
-      anonKey: 'YOUR_SUPABASE_ANON_KEY',
+      url: 'https://cvgrnnobgegbuioetkud.supabase.co',
+      anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2Z3Jubm9iZ2VnYnVpb2V0a3VkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU1MjA0MDIsImV4cCI6MjA4MTA5NjQwMn0.jeznMcCyf1unOYrKpm8ayf4jwVb_kGoWFDGRZT_QJqw',
     );
   }
 }
