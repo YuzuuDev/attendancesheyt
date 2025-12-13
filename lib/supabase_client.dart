0889315c849f84fc40ce3f54f3706545b22e58ae
YuzuuDev <ddscarletdd@gmail.com>
2025-12-13 09:03:06 +0800

    Update supabase_client.dart

--- lib/supabase_client.dart
+++ lib/supabase_client.dart
@@ -8,5 +8,6 @@ class SupabaseClientInstance {
       url: 'https://cvgrnnobgegbuioetkud.supabase.co',
       anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2Z3Jubm9iZ2VnYnVpb2V0a3VkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU1MjA0MDIsImV4cCI6MjA4MTA5NjQwMn0.jeznMcCyf1unOYrKpm8ayf4jwVb_kGoWFDGRZT_QJqw',
     );
+    supabase = Supabase.instance.client;
   }
 }
