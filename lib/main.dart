5265a9a00da9dbd121d4affc80acca568d9d81a4
YuzuuDev <ddscarletdd@gmail.com>
2025-12-13 08:51:10 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,8 @@
+import 'package:flutter/material.dart';                // For Flutter widgets
+import 'supabase_client.dart';                        // Your Supabase init
+import 'screens/login_screen.dart';                           // Login page
+import 'screens/home_screen.dart';   
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SupabaseClientInstance.init();
