423e40bdbd87939ea21e410a478f52f9eca862df
YuzuuDev <ddscarletdd@gmail.com>
2025-12-13 09:11:44 +0800

    Update supabase_client.dart

--- lib/supabase_client.dart
+++ lib/supabase_client.dart
@@ -1,13 +1,13 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupabaseClientInstance {
-  static final supabase = Supabase.instance.client;
+    // Only getter
+  static SupabaseClient get supabase => Supabase.instance.client;
 
   static Future<void> init() async {
     await Supabase.initialize(
       url: 'https://cvgrnnobgegbuioetkud.supabase.co',
       anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2Z3Jubm9iZ2VnYnVpb2V0a3VkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU1MjA0MDIsImV4cCI6MjA4MTA5NjQwMn0.jeznMcCyf1unOYrKpm8ayf4jwVb_kGoWFDGRZT_QJqw',
     );
-    supabase = Supabase.instance.client;
   }
 }
