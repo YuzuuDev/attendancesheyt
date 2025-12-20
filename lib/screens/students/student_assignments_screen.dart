fc3a8b096a712bc108d28c799f8443c56f586a2d
YuzuuDev <ddscarletdd@gmail.com>
2025-12-20 11:16:16 +0800

    Update student_assignments_screen.dart

--- lib/screens/students/student_assignments_screen.dart
+++ lib/screens/students/student_assignments_screen.dart
@@ -1,72 +1,42 @@
-import 'dart:io';
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import '../../services/assignment_service.dart';
+import 'submit_assignment_screen.dart';
 
-class SubmitAssignmentScreen extends StatefulWidget {
-  final String assignmentId;
-  final String title;
+class StudentAssignmentsScreen extends StatefulWidget {
+  final String classId;
 
-  const SubmitAssignmentScreen({
-    required this.assignmentId,
-    required this.title,
+  const StudentAssignmentsScreen({
     super.key,
+    required this.classId,
   });
 
   @override
-  State<SubmitAssignmentScreen> createState() =>
-      _SubmitAssignmentScreenState();
+  State<StudentAssignmentsScreen> createState() =>
+      _StudentAssignmentsScreenState();
 }
 
-class _SubmitAssignmentScreenState extends State<SubmitAssignmentScreen> {
-  final AssignmentService service = AssignmentService();
-  File? file;
-  bool loading = false;
+class _StudentAssignmentsScreenState
+    extends State<StudentAssignmentsScreen> {
+  final AssignmentService _service = AssignmentService();
+  bool loading = true;
+  List<Map<String, dynamic>> assignments = [];
 
-  Future<void> _pickFile() async {
-    final res = await FilePicker.platform.pickFiles();
-    if (res != null) {
-      setState(() => file = File(res.files.single.path!));
-    }
+  @override
+  void initState() {
+    super.initState();
+    _loadAssignments();
   }
 
-  Future<void> _submit() async {
-    if (file == null) return;
-
-    setState(() => loading = true);
-    await service.submitAssignment(
-      assignmentId: widget.assignmentId,
-      file: file!,
-    );
-    setState(() => loading = false);
-
-    Navigator.pop(context);
+  Future<void> _loadAssignments() async {
+    final res = await _service.getAssignments(widget.classId);
+    setState(() {
+      assignments = res;
+      loading = false;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(widget.title)),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            ElevatedButton(
-              onPressed: _pickFile,
-              child: const Text("Pick File"),
-            ),
-            if (file != null)
-              Text(file!.path.split('/').last),
-            const SizedBox(height: 20),
-            loading
-                ? const CircularProgressIndicator()
-                : ElevatedButton(
-                    onPressed: _submit,
-                    child: const Text("Submit"),
-                  ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+      appBar: AppBar(title: const Text('Assignments')),
+      body: loa
