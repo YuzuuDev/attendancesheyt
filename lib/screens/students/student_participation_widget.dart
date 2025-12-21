ed6f3c7d7d2fb0094f4ffc55dad2ed07371ac652
YuzuuDev <ddscarletdd@gmail.com>
2025-12-22 07:42:26 +0800

    Create student_participation_widget.dart

---
+++ lib/screens/students/student_participation_widget.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import '../../services/participation_service.dart';
+import '../../supabase_client.dart';
+
+class StudentParticipationWidget extends StatefulWidget {
+  @override
+  State<StudentParticipationWidget> createState() =>
+      _StudentParticipationWidgetState();
+}
+
+class _StudentParticipationWidgetState
+    extends State<StudentParticipationWidget> {
+  final ParticipationService service = ParticipationService();
+  int points = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _load();
+  }
+
+  Future<void> _load() async {
+    final id = SupabaseClientInstance.supabase.auth.currentUser!.id;
+    points = await service.getStudentPoints(id);
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: const Text("Participation Points"),
+        trailing: Text(points.toString(),
+            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+      ),
+    );
+  }
+}
