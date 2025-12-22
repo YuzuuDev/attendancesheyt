f0aa51ec711e25d6026b71385a4d9fc26e2b69fd
YuzuuDev <ddscarletdd@gmail.com>
2025-12-22 19:05:11 +0800

    Update create_assignment_screen.dart

--- lib/screens/teachers/create_assignment_screen.dart
+++ lib/screens/teachers/create_assignment_screen.dart
@@ -1,4 +1,91 @@
+import 'dart:io';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import '../../services/assignment_service.dart';
+
+class CreateAssignmentScreen extends StatefulWidget {
+  final String classId;
+  const CreateAssignmentScreen({required this.classId, super.key});
+
+  @override
+  State<CreateAssignmentScreen> createState() =>
+      _CreateAssignmentScreenState();
+}
+
+class _CreateAssignmentScreenState extends State<CreateAssignmentScreen> {
+  final service = AssignmentService();
+  final titleCtrl = TextEditingController();
+  final descCtrl = TextEditingController();
+  DateTime? dueDate;
+  File? instructionFile;
+  String type = 'activity';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Create Assignment")),
+      body: ListView(
+        padding: const EdgeInsets.all(16),
+        children: [
+          TextField(controller: titleCtrl, decoration: const InputDecoration(labelText: "Title")),
+          TextField(controller: descCtrl, decoration: const InputDecoration(labelText: "Description")),
+
+          const SizedBox(height: 12),
+
+          ElevatedButton.icon(
+            icon: const Icon(Icons.attach_file),
+            label: const Text("Attach Instruction File"),
+            onPressed: () async {
+              final res = await FilePicker.platform.pickFiles();
+              if (res != null) {
+                setState(() => instructionFile = File(res.files.single.path!));
+              }
+            },
+          ),
+
+          if (instructionFile != null)
+            Padding(
+              padding: const EdgeInsets.only(top: 6),
+              child: Text(instructionFile!.path.split('/').last),
+            ),
+
+          const SizedBox(height: 12),
+
+          ElevatedButton(
+            child: const Text("Pick Due Date"),
+            onPressed: () async {
+              final d = await showDatePicker(
+                context: context,
+                firstDate: DateTime.now(),
+                lastDate: DateTime(2100),
+              );
+              if (d != null) setState(() => dueDate = d);
+            },
+          ),
+
+          const SizedBox(height: 20),
+
+          ElevatedButton(
+            onPressed: () async {
+              await service.createAssignment(
+                classId: widget.classId,
+                title: titleCtrl.text,
+                description: descCtrl.text,
+                dueDate: dueDate,
+                assignmentType: type,
+                instructionFile: instructionFile,
+              );
+              Navigator.pop(context);
+            },
+            child: const Text("Create Assignment"),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+/*import 'package:flutter/material.dart';
 import '../../services/assignment_service.dart';
 
 class CreateAssignmentScreen extends StatefulWidget {
@@ -61,85 +148,6 @@ class _CreateAssignmentScreenState extends State<CreateAssignmentScreen> {
               child: const Text("Pick Due Date"),
             ),
             ElevatedButton(onPressed: _create, child: const Text("Create")),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-/*import 'package:flutter/material.dart';
-import '../../services/assignment_service.dart';
-
-class CreateAssignmentScreen extends StatefulWidget {
-  final String classId;
-
-  const CreateAssignmentScreen({
-    required this.classId,
-    super.key,
-  });
-
-  @override
-  State<CreateAssignmentScreen> createState() =>
-      _CreateAssignmentScreenState();
-}
-
-class _CreateAssignmentScreenState extends State<CreateAssignmentScreen> {
-  final AssignmentService service = AssignmentService();
-  final titleCtrl = TextEditingController();
-  final descCtrl = TextEditingController();
-  DateTime? dueDate;
-  bool loading = false;
-
-  Future<void> _create() async {
-    setState(() => loading = true);
-
-    await service.createAssignment(
-      classId: widget.classId,
-      title: titleCtrl.text,
-      description: descCtrl.text,
-      dueDate: dueDate,
-    );
-
-    setState(() => loading = false);
-    Navigator.pop(context);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Create Assignment")),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            TextField(
-              controller: titleCtrl,
-              decoration: const InputDecoration(labelText: "Title"),
-            ),
-            TextField(
-              controller: descCtrl,
-              decoration: const InputDecoration(labelText: "Description"),
-            ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () async {
-                final d = await showDatePicker(
-                  context: context,
-                  firstDate: DateTime.now(),
-                  lastDate: DateTime(2100),
-                );
-                if (d != null) setState(() => dueDate = d);
-              },
-              child: const Text("Pick Due Date"),
-            ),
-            const SizedBox(height: 20),
-            loading
-                ? const CircularProgressIndicator()
-                : ElevatedButton(
-                    onPressed: _create,
-                    child: const Text("Create"),
-                  ),
           ],
         ),
       ),
