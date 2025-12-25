7c2c2271741e2656a338f026f5ce15afcf737291
YuzuuDev <ddscarletdd@gmail.com>
2025-12-25 12:22:07 +0800

    Update index.ts

--- supabase/functions/notify-participation/index.ts
+++ supabase/functions/notify-participation/index.ts
@@ -14,8 +14,8 @@ serve(async (req) => {
 
   const supabase = await import("npm:@supabase/supabase-js");
   const client = supabase.createClient(
-    Deno.env.get("SUPABASE_URL")!,
-    Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!
+    Deno.env.get("URL")!,
+    Deno.env.get("SERVICE_ROLE_KEY")!
   );
 
   const { data: tokens } = await client
