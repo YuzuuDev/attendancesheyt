3de5b03f855aa94d52b7ba936e2e002ba3ca01d2
YuzuuDev <ddscarletdd@gmail.com>
2025-12-25 19:36:29 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'app_theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await SupabaseClientInstance.init();
 
   final isLoggedIn =
