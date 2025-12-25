a62e6a54e699061ad1805db4f2f1452775bb1692
YuzuuDev <ddscarletdd@gmail.com>
2025-12-25 17:53:25 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,13 @@ import 'screens/login_screen.dart';
 import 'screens/home_screen.dart';
 import 'supabase_client.dart';
 import 'app_theme.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SupabaseClientInstance.init();
+  //initizalize OneSignal / pushing notifications
+  await initOneSignal();
 
   final isLoggedIn =
       SupabaseClientInstance.supabase.auth.currentUser != null;
@@ -25,7 +28,12 @@ class MyApp extends StatelessWidget {
     required this.isLoggedIn,
     super.key,
   });
-
+  
+  Future<void> initOneSignal() async {
+    OneSignal.initialize('ONESIGNAL_APP_ID');
+    OneSignal.Notifications.requestPermission(true);
+  }
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
