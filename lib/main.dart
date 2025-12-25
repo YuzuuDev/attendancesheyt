061db69a0c0da6e5cbcbb7b0310396edec01cb87
YuzuuDev <ddscarletdd@gmail.com>
2025-12-25 15:50:39 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,29 +4,27 @@ import 'screens/home_screen.dart';
 import 'supabase_client.dart';
 import 'app_theme.dart';
 
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  // ✅ FIREBASE CORE ONLY — SAFE
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
-  // ✅ SUPABASE AFTER FIREBASE
   await SupabaseClientInstance.init();
 
   final isLoggedIn =
       SupabaseClientInstance.supabase.auth.currentUser != null;
 
-  runApp(MyApp(isLoggedIn: isLoggedIn));
+  runApp(
+    MyApp(
+      isLoggedIn: isLoggedIn,
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   final bool isLoggedIn;
-  const MyApp({required this.isLoggedIn, super.key});
+
+  const MyApp({
+    required this.isLoggedIn,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -38,50 +36,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-
-/*import 'package:flutter/material.dart';
-import 'screens/login_screen.dart';
-import 'screens/home_screen.dart';
-import 'supabase_client.dart';
-import 'app_theme.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'services/push_notification_service.dart';
-
-
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await SupabaseClientInstance.init();
-
-  final isLoggedIn =
-      SupabaseClientInstance.supabase.auth.currentUser != null;
-  
-  await Firebase.initializeApp();
-
-    // 🔴 REQUIRED FOR ANDROID 13+ AND IOS
-  await FirebaseMessaging.instance.requestPermission(
-    alert: true,
-    badge: true,
-    sound: true,
-  );
-  
-  runApp(MyApp(isLoggedIn: isLoggedIn));
-}
-
-class MyApp extends StatelessWidget {
-  final bool isLoggedIn;
-  const MyApp({required this.isLoggedIn, super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Smart Attendance',
-      theme: AppTheme.theme,
-      home: isLoggedIn ? HomeScreen() : LoginScreen(),
-    );
-  }
-}
-*/
